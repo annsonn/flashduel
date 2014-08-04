e0c51dcfe1feb7079c3c3382f98c5e80619e2786
anson-chung <anson.chung@intelliware.com>
2014-08-04 00:33:02 -0400

    placing players and making them move

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -2,6 +2,22 @@
 
 function MainController($scope) {
   $scope.title = 'Flash Duel';
+
+  $scope.players = [{
+    name: 'Player 1',
+    color: 'red',
+    position: 1,
+    direction: 1
+  }, {
+    name: 'Player 2',
+    color: 'blue',
+    position: 18,
+    direction: -1
+  }];
+
+  $scope.move = function move(player, spaces) {
+    player.position = player.position + (player.direction * spaces);
+  };
 }
 angular
     .module('app', [])
