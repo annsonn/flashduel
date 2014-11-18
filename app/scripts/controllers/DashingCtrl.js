075177299de1cab701f2ef35bf9b991ffcebbd9e
Anson Chung <anson.chung@gmail.com>
2014-11-18 16:19:13 -0500

    creating controllers for each tab

---
+++ app/scripts/controllers/DashingCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ansonchung on 14-11-18.
+ */
+'use strict';
+
+angular.module('app').controller('DashingCtrl', function($scope, $log) {
+
+});
---
+++ app/scripts/controllers/MoveCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('app').controller('MoveCtrl', function($scope, $log) {
+
+});
